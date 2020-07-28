c76b306e7bd9228137f1a8d5a750941991a70c9d
Amena <amena.foshanji@gmail.com>
2020-07-28 19:06:31 -0400

    Initial Commit

---
+++ Tip1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip1
+//
+//  Created by Nazir Ahamad on 7/28/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
